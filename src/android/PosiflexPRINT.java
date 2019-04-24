5fbf8637e54e64017d870c2f4d81f5f971b38805
Thembinkosi <thembinkosi.klein@gmail.com>
2019-04-24 14:43:43 +0200

    updates

--- src/android/PosiflexPRINT.java
+++ src/android/PosiflexPRINT.java
@@ -1,20 +1,13 @@
 package cordova.plugin.posiflexprint;
 
+import com.pos.printer.*;
+
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import com.pos.printer.*;
-
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.Spinner;
 
 /**
  * This class echoes a string called from JavaScript.
